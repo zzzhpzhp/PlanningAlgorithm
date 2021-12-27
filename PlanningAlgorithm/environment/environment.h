624d8d307b231b95489f6c8e00c8c3b3b7c54eb0
zzzhpzhp <917309156@qq.com>
2021-12-27 14:34:16 +0800

    递增环境版本号。

--- PlanningAlgorithm/environment/environment.h
+++ PlanningAlgorithm/environment/environment.h
@@ -20,7 +20,7 @@ namespace environment
     class Environment : public EnvironmentInterface
     {
     public:
-        const std::string ENVIRONMENT_VERSION{"V1.2"};
+        const std::string ENVIRONMENT_VERSION{"V1.3"};
 
         void
         initialize(int length, int width, int display_scale) override;
